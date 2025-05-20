cda4f423c9873c63637001d5f8108781c1042c7b
William White <whw@bpmmicro.com>
2025-05-20 09:06:20 -0700

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+const CACHE = "breath-cycle-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
